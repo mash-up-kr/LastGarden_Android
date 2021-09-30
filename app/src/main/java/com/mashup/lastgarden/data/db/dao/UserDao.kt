8cee785e66e5701048a12ea6653a016f8a0187ae
Jaemin.Park <once29@naver.com>
2021-10-01 03:11:56 +0900

    🚀 Implement BaseDao

--- app/src/main/java/com/mashup/lastgarden/data/db/dao/UserDao.kt
+++ app/src/main/java/com/mashup/lastgarden/data/db/dao/UserDao.kt
@@ -1,19 +1,12 @@
 package com.mashup.lastgarden.data.db.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
 import androidx.room.Query
 import com.mashup.lastgarden.data.vo.User
 
 @Dao
-interface UserDao {
-    @Insert
-    fun insert(user: User)
-
-    @Delete
-    fun delete(user: User)
+abstract class UserDao : BaseDao<User>() {
 
     @Query("SELECT * FROM users WHERE id = :userId")
-    fun getUser(userId: String): User
+    abstract fun getUser(userId: String): User
 }
