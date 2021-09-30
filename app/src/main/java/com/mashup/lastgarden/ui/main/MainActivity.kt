c5239c6a99d7930654c9da6e2c96e2221212fd88
Jaemin.Park <once29@naver.com>
2021-10-01 02:59:33 +0900

    🚀 Setup navController in MainActivity

--- app/src/main/java/com/mashup/lastgarden/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/lastgarden/ui/main/MainActivity.kt
@@ -2,13 +2,24 @@ package com.mashup.lastgarden.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.mashup.lastgarden.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        navController = (supportFragmentManager.findFragmentById(R.id.navHostFragment)
+                as NavHostFragment).navController
     }
 }
