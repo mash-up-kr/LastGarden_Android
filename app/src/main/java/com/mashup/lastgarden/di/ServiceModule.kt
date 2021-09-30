5f5b68c1d5e95e3fd49492dbe890beb274a351ef
Jaemin.Park <once29@naver.com>
2021-10-01 02:50:54 +0900

    🚀 Implement ServiceModule

---
+++ app/src/main/java/com/mashup/lastgarden/di/ServiceModule.kt
@@ -0,0 +1,18 @@
+package com.mashup.lastgarden.di
+
+import com.mashup.base.network.PerfumeRetrofit
+import com.mashup.lastgarden.network.services.UserService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+
+    @Provides
+    fun provideUserService(okHttpClient: OkHttpClient): UserService =
+        PerfumeRetrofit.create(okHttpClient)
+}
