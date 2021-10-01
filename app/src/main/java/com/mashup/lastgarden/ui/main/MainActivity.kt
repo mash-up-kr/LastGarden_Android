55e8c0bdeb3bd5a309c541e71aa25ede9d9dca28
Jaemin.Park <once29@naver.com>
2021-10-01 22:35:44 +0900

    WIP

--- app/src/main/java/com/mashup/lastgarden/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/lastgarden/ui/main/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
         setupNavController()
     }
 
