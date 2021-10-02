9dd25c14fc3a18ca3b1f0867420fedc0673bbba3
ymcho24 <ymcho24@naver.com>
2021-10-03 00:46:35 +0900

    :rocket: Implement perfume detail and perfume information layout

--- app/src/main/java/com/mashup/lastgarden/ui/main/PerfumeDetailFragment.kt
+++ app/src/main/java/com/mashup/lastgarden/ui/main/PerfumeDetailFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.mashup.base.autoCleared
+import com.mashup.lastgarden.R
 import com.mashup.lastgarden.databinding.FragmentPerfumeDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class PerfumeDetailFragment : Fragment() {
 
     private var binding by autoCleared<FragmentPerfumeDetailBinding>()
+    private lateinit var viewPagerAdapter: PerfumeDetailPagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +25,22 @@ class PerfumeDetailFragment : Fragment() {
         binding = FragmentPerfumeDetailBinding.inflate(
             inflater, container, false
         )
+        initTabLayout()
         return binding.root
+    }
+
+    private fun initTabLayout() {
+        val tabIconList = listOf(
+            R.drawable.tab_icon_perfume_selector, R.drawable.tab_icon_squares_four_selector
+        )
+        viewPagerAdapter = PerfumeDetailPagerAdapter(requireActivity())
+        viewPagerAdapter.fragments = listOf(
+            PerfumeInformationFragment(),
+            ScentListFragment()
+        )
+        binding.viewPager.adapter = viewPagerAdapter
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.setIcon(tabIconList[position])
+        }.attach()
     }
 }
