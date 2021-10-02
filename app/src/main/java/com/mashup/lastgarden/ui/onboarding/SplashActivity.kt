9d19859ca031830678629476b683c2355b522d4e
jaeryo <jaeryo@cleaninglab.co.kr>
2021-10-02 21:38:20 +0900

    :building_construction: LifecycleScope로 변경

--- app/src/main/java/com/mashup/lastgarden/ui/onboarding/SplashActivity.kt
+++ app/src/main/java/com/mashup/lastgarden/ui/onboarding/SplashActivity.kt
@@ -1,29 +1,24 @@
 package com.mashup.lastgarden.ui.onboarding
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.mashup.lastgarden.R
-import com.mashup.lastgarden.ui.main.MainActivity
-import kotlinx.coroutines.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
-    private val activityScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        activityScope.launch {
+        lifecycleScope.launch {
             delay(3000)
 
             val intent = Intent(this@SplashActivity, OnBoardingActivity::class.java)
             startActivity(intent)
             finish()
         }
-    }
-
-    override fun onPause() {
-        activityScope.cancel()
-        super.onPause()
     }
 }
