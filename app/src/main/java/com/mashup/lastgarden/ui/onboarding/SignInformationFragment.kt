f9fed98a268fa09fe18c1c9323d6140f370ef11b
ymcho24 <ymcho24@naver.com>
2021-10-02 14:41:16 +0900

    :rocket: #18 Implement sign information and sign complete layout

---
+++ app/src/main/java/com/mashup/lastgarden/ui/onboarding/SignInformationFragment.kt
@@ -0,0 +1,27 @@
+package com.mashup.lastgarden.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mashup.base.autoCleared
+import com.mashup.lastgarden.databinding.FragmentSignInformationBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignInformationFragment : Fragment() {
+
+    private var binding by autoCleared<FragmentSignInformationBinding>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSignInformationBinding.inflate(
+            inflater, container, false
+        )
+        return binding.root
+    }
+}
