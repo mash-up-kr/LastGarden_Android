bdef4c72f71baafe0d7f178b0cfc423f5599aec0
ymcho24 <ymcho24@naver.com>
2021-10-12 02:19:28 +0900

    :rocket: Implement perfume information layout

--- app/src/main/java/com/mashup/lastgarden/ui/main/PerfumeInformationFragment.kt
+++ app/src/main/java/com/mashup/lastgarden/ui/main/PerfumeInformationFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mashup.base.autoCleared
+import com.mashup.lastgarden.R
 import com.mashup.lastgarden.databinding.FragmentPerfumeInformationBinding
 import com.mashup.lastgarden.ui.BaseViewModelFragment
 
@@ -19,6 +20,42 @@ class PerfumeInformationFragment : BaseViewModelFragment() {
         binding = FragmentPerfumeInformationBinding.inflate(
             inflater, container, false
         )
+        setNoteButtonChecked()
         return binding.root
+    }
+
+    private fun setNoteButtonChecked() {
+        binding.checkBoxTop.setOnClickListener {
+            allButtonUnchecked()
+            binding.apply {
+                checkBoxTop.isChecked = true
+                imageViewPyramid.setImageResource(R.drawable.ic_pyramid_top)
+                textViewPyramidContent.text = "시트러스 어코드, 페티그레인, 블랙커런트, 레드프룻, 피치"
+            }
+        }
+        binding.checkBoxMiddle.setOnClickListener {
+            allButtonUnchecked()
+            binding.apply {
+                checkBoxMiddle.isChecked = true
+                imageViewPyramid.setImageResource(R.drawable.ic_pyramid_middle)
+                textViewPyramidContent.text = "화이트플라워, 튜베로즈, 재스민, 일랑일랑, 오렌지블로썸, 코코넛"
+            }
+        }
+        binding.checkBoxBase.setOnClickListener {
+            allButtonUnchecked()
+            binding.apply {
+                checkBoxBase.isChecked = true
+                imageViewPyramid.setImageResource(R.drawable.ic_pyramid_base)
+                textViewPyramidContent.text = "샌달우드, 바닐라, 화이트머스크"
+            }
+        }
+    }
+
+    private fun allButtonUnchecked() {
+        binding.apply {
+            checkBoxTop.isChecked = false
+            checkBoxMiddle.isChecked = false
+            checkBoxBase.isChecked = false
+        }
     }
 }
