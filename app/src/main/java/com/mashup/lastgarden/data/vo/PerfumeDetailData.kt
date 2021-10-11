5a7501c2377297d7d5695f7e0dbf412072293d85
ymcho24 <ymcho24@naver.com>
2021-10-12 02:55:53 +0900

    :rocket: Implement scent list layout

---
+++ app/src/main/java/com/mashup/lastgarden/data/vo/PerfumeDetailData.kt
@@ -0,0 +1,9 @@
+package com.mashup.lastgarden.data.vo
+
+data class PerfumeDetailData(
+    val photo: String,
+    val profileImage: String,
+    val name: String,
+    val tags: String,
+    val likeCount: Long
+)
