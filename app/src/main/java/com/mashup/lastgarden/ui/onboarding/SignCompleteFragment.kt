af39598ca549c14ad9b1f17d5bf8dffc125ed936
ymcho24 <ymcho24@naver.com>
2021-10-17 00:24:07 +0900

    :building_construction: Edit toolbar layout and define check box shape

--- app/src/main/java/com/mashup/lastgarden/ui/onboarding/SignCompleteFragment.kt
+++ app/src/main/java/com/mashup/lastgarden/ui/onboarding/SignCompleteFragment.kt
@@ -1,10 +1,13 @@
 package com.mashup.lastgarden.ui.onboarding
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.mashup.base.autoCleared
+import com.mashup.lastgarden.R
 import com.mashup.lastgarden.databinding.FragmentSignCompleteBinding
 import com.mashup.lastgarden.ui.BaseViewModelFragment
 
@@ -13,13 +16,25 @@ class SignCompleteFragment : BaseViewModelFragment() {
     private var binding by autoCleared<FragmentSignCompleteBinding>()
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         binding = FragmentSignCompleteBinding.inflate(
-                inflater, container, false
+            inflater, container, false
         )
         return binding.root
+    }
+
+    override fun onSetupViews(view: View) {
+        super.onSetupViews(view)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.toolbar.setNavigationIcon(R.drawable.ic_close)
+        binding.toolbar.navigationIcon?.setTint(Color.BLACK)
     }
 }
--- app/src/main/java/com/mashup/lastgarden/ui/onboarding/SignInformationFragment.kt
+++ app/src/main/java/com/mashup/lastgarden/ui/onboarding/SignInformationFragment.kt
@@ -1,10 +1,13 @@
 package com.mashup.lastgarden.ui.onboarding
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.mashup.base.autoCleared
+import com.mashup.lastgarden.R
 import com.mashup.lastgarden.databinding.FragmentSignInformationBinding
 import com.mashup.lastgarden.ui.BaseViewModelFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,5 +26,17 @@ class SignInformationFragment : BaseViewModelFragment() {
             inflater, container, false
         )
         return binding.root
+    }
+
+    override fun onSetupViews(view: View) {
+        super.onSetupViews(view)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.toolbar.setNavigationIcon(R.drawable.ic_close)
+        binding.toolbar.navigationIcon?.setTint(Color.BLACK)
     }
 }
