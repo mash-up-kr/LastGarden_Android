ba2c8fcddc90822e2a965717608abc8c20b5619c
leejieun1121 <lje4648@naver.com>
2021-10-19 19:43:58 +0900

    :building_construction: Splash Logo Glide로 변경

--- app/src/main/java/com/mashup/lastgarden/ui/onboarding/SplashActivity.kt
+++ app/src/main/java/com/mashup/lastgarden/ui/onboarding/SplashActivity.kt
@@ -4,14 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.mashup.base.image.GlideApp
 import com.mashup.lastgarden.R
+import com.mashup.lastgarden.databinding.ActivitySplashBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        GlideApp.with(this)
+            .load(R.drawable.ic_splash_logo)
+            .into(binding.splashImageView)
+
         lifecycleScope.launch {
             delay(3000)
 
