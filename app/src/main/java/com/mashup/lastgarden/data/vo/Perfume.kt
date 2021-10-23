1ff2f2dd8f22c5013ddc3397b0e166b554705ff2
Jaemin.Park <once29@naver.com>
2021-10-23 23:53:21 +0900

    🚀 Add Perfume

--- app/src/main/java/com/mashup/lastgarden/data/vo/Perfume.kt
+++ app/src/main/java/com/mashup/lastgarden/data/vo/Perfume.kt
@@ -1,16 +1,26 @@
 package com.mashup.lastgarden.data.vo
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "perfumes")
 data class Perfume(
-    @PrimaryKey val id: String,
-    val name: String,
-    val brandName: String,
-    val stories: List<Story>? = null,
-    val perfumeImage: String? = null,
-    val likeCount: Long? = 0L
+    @PrimaryKey @ColumnInfo(name = "perfume_id") @SerializedName("id") val perfumeId: String,
+    @ColumnInfo(name = "perfume_name") val name: String,
+    @ColumnInfo(name = "perfume_thumbnail_url") val thumbnailUrl: String? = null,
+    @ColumnInfo(name = "perfume_korean_name") val koreanName: String,
+    @ColumnInfo(name = "perfume_brand_id") val brandId: String,
+    @ColumnInfo(name = "perfume_brand_name") val brandName: String,
+    @ColumnInfo(name = "perfume_like_count") val likeCount: Long? = 0L,
+    @Ignore val notes: NoteContainer
 ) {
-    data class Story(val id: String, val authorId: String, val title: String, val likeCount: Long? = 0L)
+    data class NoteContainer(
+        val top: List<Note>? = emptyList(),
+        val middle: List<Note>? = emptyList(),
+        val base: List<Note>? = emptyList(),
+        val default: List<Note>? = emptyList()
+    )
 }
