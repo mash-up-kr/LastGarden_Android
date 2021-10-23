e9cbe0b0b62eefa2c2a342288d1853d18ceb8d38
Jaemin.Park <once29@naver.com>
2021-10-24 02:43:41 +0900

    🏗️ Add NetworkDataResponse

--- app/src/main/java/com/mashup/lastgarden/network/response/Response.kt
+++ app/src/main/java/com/mashup/lastgarden/network/response/Response.kt
@@ -1,5 +1,8 @@
 package com.mashup.lastgarden.network.response
 
+import android.util.Log
+import com.haroldadmin.cnradapter.NetworkResponse
+
 data class Response<T>(
     val data: T? = null,
     val paging: Paging? = null,
@@ -13,3 +16,46 @@ data class Error(
     val code: Int? = null,
     val message: String? = null
 )
+
+data class DataResponse<T>(val data: T? = null)
+data class ErrorResponse(val error: Error? = null)
+
+typealias NetworkDataResponse<T> = NetworkResponse<DataResponse<T>, ErrorResponse>
+
+fun <T : Any> NetworkDataResponse<T>.onErrorReturnData(item: T): T = when (this) {
+    is NetworkResponse.Success -> body.data ?: item
+    is NetworkResponse.ServerError -> {
+        Log.e(
+            "NetworkResponse",
+            "onErrorReturn: ServerError(${this.code}), ${this.body?.error?.code}, ${this.body?.error?.message}"
+        )
+        item
+    }
+    is NetworkResponse.NetworkError -> {
+        Log.e("NetworkResponse", "onErrorReturn: NetworkError", this.error)
+        item
+    }
+    is NetworkResponse.UnknownError -> {
+        Log.e("NetworkResponse", "onErrorReturn: UnknownError(${this.code})", this.error)
+        item
+    }
+}
+
+fun <T : Any> NetworkDataResponse<T>.onErrorReturnDataNull(): T? = when (this) {
+    is NetworkResponse.Success -> body.data
+    is NetworkResponse.ServerError -> {
+        Log.e(
+            "NetworkResponse",
+            "onErrorReturn: ServerError(${this.code}), ${this.body?.error?.code}, ${this.body?.error?.message}"
+        )
+        null
+    }
+    is NetworkResponse.NetworkError -> {
+        Log.e("NetworkResponse", "onErrorReturn: NetworkError", this.error)
+        null
+    }
+    is NetworkResponse.UnknownError -> {
+        Log.e("NetworkResponse", "onErrorReturn: UnknownError(${this.code})", this.error)
+        null
+    }
+}
