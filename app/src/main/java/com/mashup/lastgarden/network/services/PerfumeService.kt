04fda5f834cee4724edac250045efeaaf341fa50
Jaemin <once29@naver.com>
2021-11-04 10:41:50 +0900

    🚀 Add fetching main elements logic in PerfumeService

--- app/src/main/java/com/mashup/lastgarden/network/services/PerfumeService.kt
+++ app/src/main/java/com/mashup/lastgarden/network/services/PerfumeService.kt
@@ -1,11 +1,24 @@
 package com.mashup.lastgarden.network.services
 
+import com.mashup.lastgarden.data.vo.Perfume
 import com.mashup.lastgarden.data.vo.PerfumeAndStories
 import com.mashup.lastgarden.network.response.NetworkDataResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PerfumeService {
 
     @GET("home/today")
     suspend fun getTodayPerfume(): NetworkDataResponse<PerfumeAndStories>
+
+    @GET("home/weekly-ranking")
+    suspend fun getWeeklyRanking(): NetworkDataResponse<WeeklyRankingResponse>
+
+    @GET("home/steady-perfume")
+    suspend fun getSteadyPerfumes(
+        @Query("idCursor") idCursor: String? = null,
+        @Query("likeCursor") likeCursor: String? = null
+    ): NetworkDataResponse<List<Perfume>>
 }
+
+data class WeeklyRankingResponse(val perfumes: List<Perfume>)
