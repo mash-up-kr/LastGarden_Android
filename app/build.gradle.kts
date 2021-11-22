01c138964d81c365769e490acd2052896d96ae12
Jaemin <once29@naver.com>
2021-11-23 00:21:50 +0900

    🏗️ Add Paging3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,4 +105,7 @@ dependencies {
     // ReadMoreTextView
     implementation("kr.co.prnd:readmore-textview:1.0.0")
 
+    // Paging3
+    implementation(Libs.AndroidX.Paging.runtime)
+    testImplementation(Libs.AndroidX.Paging.common)
 }
