9bd6c36431cbed5f1174cce08fbe56f77afdd0f1
leejieun1121 <lje4648@naver.com>
2021-11-26 14:59:16 +0900

    :art: Fix item scent layout bottom gradient

--- app/src/main/java/com/mashup/lastgarden/ui/scent/ScentViewPagerAdapter.kt
+++ app/src/main/java/com/mashup/lastgarden/ui/scent/ScentViewPagerAdapter.kt
@@ -42,7 +42,7 @@ class ScentViewPagerAdapter(
     private fun ScentViewHolder.bindImageView(item: Story) {
         binding.run {
             glideRequests.load(item.imageUrl).into(binding.scentImageView)
-            profileImageView.setImageUrl(glideRequests, item.perfumeThumbnailUrl)
+            profileImageView.setImageUrl(glideRequests, item.userProfileImage)
             commentImageView.setOnClickListener { listener?.onCommentClick(item.storyId) }
             likeImageView.setOnClickListener { listener?.onLikeClick(item.storyId) }
             likeImageView.loadImage(glideRequests, R.drawable.ic_dislike)
